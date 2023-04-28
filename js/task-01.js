367bc7e32380e9e963fa4c41db32cfe47d1fac2b
Maryna <komunarmarin@gmail.com>
2023-04-28 14:20:05 +0300

    add buttun

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,16 @@
-const categoriesli = document.querySelectorAll('.item');
+/*const categoriesli = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesli.length}`);
 categoriesli.forEach (item => {
     console.log (`Category: ${item.firstElementChild.textContent} `);
     console.log (`Elements: ${item.lastElementChild.querySelectorAll('li').length}`);
-});
+});*/
 
+
+
+const mari = document.querySelectorAll('.item');
+console.log(`Number of categories: ${mari.length}`);
+mari.forEach ( item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.querySelectorAll('li').length}`);
+
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,7 +1,132 @@
-function getRandomHexColor() {
+/*function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+}*/
+
+/*const magicBnt = document.querySelector('.js-magic-btn');
+
+magicBnt.addEventListener('click', () => {
+const images = document.querySelector('.hero__image');
+console.log('images', images);
+console.log(images.src);
+images.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+images.alt = "Це новий котик!";
+});
+
+const heroTitle = document.querySelector('.hero__title');
+console.log('heroTitle', heroTitle);
+console.log(heroTitle.textContent);
+heroTitle.textContent = ' Я солодкий булочка!';*/
+
+/*const images = document.querySelector('.hero__image');
+
+images.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+images.alt = "Це новий котик!";
+
+const heroTitle = document.querySelector('.hero__title');
+heroTitle.textContent = ' Я солодкий булочка!';
+
+console.log(images.getAttribute('src'));
+console.log(images.src);
+images.removeAttribute('src');
+console.log(images.hasAttribute('src'));*/
+
+//const actions = document.querySelectorAll('.js-actions button');
+//console.log(actions [2].dataset.action);
+
+
+
+/*const knopka = document.querySelectorAll('.js-actions button');
+console.log(knopka[1].dataset.knopka);*/
+
+
+/*const magicBnt = document.querySelector('.js-magic-btn');
+magicBnt.addEventListener('click', () => {
+  const naEl = document.querySelector('.actions');
+  console.log(naEl.classList);
+  naEl.classList.add('super-cool');
+  naEl.classList.remove('super-cool');
+});*/
+
+/*const currentPage = "/about";
+const  mary = document.querySelector(`.sate-nav__link[href= '${currentPage}']`);
+console.log( mary);
+mary.classList.add('sate-nav__link--cur');*/
+
+/*const title = document.createElement('h1');
+title.classList.add('page-title');
+title.textContent = 'Це загоовок сторінки :)';
+console.log(title);
+
+document.body.appendChild(title);*/
+
+
+/*const mari = document.createElement('h1');
+mari.classList.add('sate-nav');
+mari.textContent = 'Маріна все вийде у тебе в IT';
+console.log( mari);
+document.body.appendChild(mari);*/
+
+/*const imageEl = document.createElement('img');
+imageEl.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageEl.alt = 'Group of Horses Running';
+
+imageEl.width = 640;
+console.log('imageEl', imageEl);
+
+document.body.appendChild(imageEl);*/
+
+/*const imageEl = document.createElement('img');
+imageEl.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageEl.alt = 'Group of Horses Running';
+
+imageEl.width = 640;
+console.log('imageEl', imageEl);
+
+document.body.appendChild(imageEl);
+imageEl.insertBefore()*/
+
+/*const colorPickerOption = [
+  {label: 'red', 'color': '#443336'},
+  {label: 'green', 'color': '#443336'},
+  {label: 'blue', 'color': '#443336'},
+  {label: 'grey', 'color': '#443336'},
+  {label: 'pink', 'color': '#443336'},
+];
+const option = colorPickerOption[4];
+
+const buttonEl = document.createElement('button');
+buttonEl.type = 'button';
+buttonEl.textContent = option.label;
+buttonEl.style.backgroundColor = option.color;
+
+console.log(buttonEl);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
